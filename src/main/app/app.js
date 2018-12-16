705dc8dd55815309eaa93a17fbe5b7e333492fb6
Sergey <sdaleshin@gmail.com>
2018-12-16 15:35:05 +0300

    new favicon

--- src/main/app/app.js
+++ src/main/app/app.js
@@ -8,6 +8,8 @@ import Favicon from 'react-favicon';
 import {ConnectedTestPage} from './pages/test/TestPage';
 import {Provider} from 'react-redux';
 import {configureStore} from './store/configureStore';
+import {Helmet} from 'react-helmet';
+import {ThemeProvider} from './components/theme-context/ThemeContext';
 
 
 export class App extends React.Component {
@@ -26,7 +28,17 @@ export class App extends React.Component {
           <Route path='/' component={HomePage}/>
         </Switch>
       </Provider>,
-      <Favicon key="2" url={require('./images/favicon.ico')}/>
+      <Helmet>
+        <link rel="icon" sizes="192x192" href={require('./images/favicon-192x192.png')}/>
+        <link rel="icon" sizes="144x144" href={require('./images/favicon-144x144.png')}/>
+        <link rel="icon" sizes="96x96" href={require('./images/favicon-96x96.png')}/>
+        <link rel="icon" sizes="48x48" href={require('./images/favicon-48x48.png')}/>
+        <link rel="apple-touch-icon" href={require('./images/favicon-192x192.png')}/>
+        <link rel="apple-touch-icon" sizes="76x76" href={require('./images/favicon-76x76.png')}/>
+        <link rel="apple-touch-icon" sizes="120x120" href={require('./images/favicon-120x120.png')}/>
+        <link rel="apple-touch-icon" sizes="152x152" href={require('./images/favicon-152x152.png')}/>
+        <meta name="msapplication-square310x310logo" content={require('./images/favicon-310x310.png')}/>
+      </Helmet>
     ]);
   }
 }
